464a24910bd647771ffc30f5a89bab88ec687ed7
Sukhbat A - laptop <ssuheesh@gmail.com>
2025-01-28 14:00:01 -0600

    update #3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello From the Other siiiiide.</h1>
+      <h1>Hello From the Other siiiiide. SMD!</h1>
     </div>
   );
 }
